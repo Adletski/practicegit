e4a0628a16b3261b82a3d4d8d5a35b53a9f023d7
Adlet Zhantassov <Adlet@MacBook-Pro-Techman.local>
2023-12-13 12:34:28 +0600

    Revert "commit 4"
    This reverts commit e9b1c903258bb81238b9838ac504dcccbf89f3d9.

--- pracitcegit/ViewController.swift
+++ pracitcegit/ViewController.swift
@@ -15,8 +15,6 @@ class ViewController: UIViewController {
         print("hello there")
         
         print("second heello here for practicing git")
-        
-        print("hello fourth commit")
     }
 
 
